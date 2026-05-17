a57d7e9421ff69b8431ac396aac39202b3a501a2
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2026-05-17 23:21:51 +0200

    some edits 🗿

--- include/Config.hpp
+++ include/Config.hpp
@@ -5,7 +5,7 @@
 /*│  By: 0xK92JL4                                               ▒▒▒▒          │*/
 /*│                                                           ▒▒▒▒▒▒▒▒        │*/
 /*│  Created: 2026/05/17 00:58:04 by 0xK92JL4                 ▒▒▒▒▒▒▒▒        │*/
-/*│  Updated: 2026/05/17 00:58:27 by 0xK92JL4                 ▒▒    ▒▒        │*/
+/*│  Updated: 2026/05/17 23:21:48 by 0xK92JL4                 ▒▒    ▒▒        │*/
 /*│                                                                           │*/
 /*└───────────────────────────────────────────────────────────────────────────┘*/
 
@@ -14,17 +14,18 @@
 #include <unordered_map>
 #include <linux/input.h>
 
-namespace Config {
+namespace Config
+{
     constexpr int DEADZONE = 20;
     constexpr int MAX_AXIS_RANGE = 127;
 
-    // Sensitivities calibrated for millisecond delta-time tracking
     constexpr float MOUSE_SENS_X = 1.0f;
     constexpr float MOUSE_SENS_Y = 1.0f;
 
     constexpr float SCROLL_SENS_X = 0.035f; 
     constexpr float SCROLL_SENS_Y = 0.020f; 
-
+ 
+	// grep -H . /sys/class/input/event*/device/name
     const char* const DS4_DEVICE = "/dev/input/event21";
     const char* const TOUCHPAD_DEVICE = "/dev/input/event23";
 
