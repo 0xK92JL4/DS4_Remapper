b92dda68cb7ff483e1eff703d638e113105e2925
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2026-05-30 19:50:44 +0200

    style(config): cleaner command look

--- include/Config.hpp
+++ include/Config.hpp
@@ -5,7 +5,7 @@
 /*│  By: 0xK92JL4                                               ▒▒▒▒          │*/
 /*│                                                           ▒▒▒▒▒▒▒▒        │*/
 /*│  Created: 2026/05/17 00:58:04 by 0xK92JL4                 ▒▒▒▒▒▒▒▒        │*/
-/*│  Updated: 2026/05/30 19:41:12 by 0xK92JL4                 ▒▒    ▒▒        │*/
+/*│  Updated: 2026/05/30 19:49:39 by 0xK92JL4                 ▒▒    ▒▒        │*/
 /*│                                                                           │*/
 /*└───────────────────────────────────────────────────────────────────────────┘*/
 
@@ -16,6 +16,13 @@
 #include <linux/input.h>
 #include <cstdint>
 #include <chrono>
+
+namespace Command
+{
+	static constexpr const char* TOGGLE_SVKBD =
+		"pgrep -x svkbd-en >/dev/null && pkill -x svkbd-en"
+		" || svkbd-en -g 1536x356+192+400 >/dev/null 2>&1 &";
+}
 
 namespace Config
 {
@@ -38,10 +45,10 @@ namespace Config
     const char* const DS4_DEVICE = "/dev/input/event21";
     const char* const TOUCHPAD_DEVICE = "/dev/input/event23";
 
-	const char* const LED_R = "/sys/class/leds/input27:red/brightness";
-	const char* const LED_G = "/sys/class/leds/input27:green/brightness";
-	const char* const LED_B = "/sys/class/leds/input27:blue/brightness";
-	const char* const LED_GLOBAL = "/sys/class/leds/input27:global/brightness";
+	const char* const LED_R = "/sys/class/leds/input50:red/brightness";
+	const char* const LED_G = "/sys/class/leds/input50:green/brightness";
+	const char* const LED_B = "/sys/class/leds/input50:blue/brightness";
+	const char* const LED_GLOBAL = "/sys/class/leds/input50:global/brightness";
 
 	const char* const BATTERY_CAPACITY
 		= "/sys/class/power_supply/ps-controller-battery-28:c1:3c:48:43:83/capacity";
@@ -58,10 +65,10 @@ namespace Config
 		{ BTN_EAST,    MouseButton(BTN_RIGHT)      },
 		{ BTN_NORTH,   MouseButton(BTN_MIDDLE)     },
 
-		{ BTN_WEST,   Command({"/usr/bin/sh", "-c", "pgrep -x svkbd-en >/dev/null && pkill -x svkbd-en || svkbd-en -g 1536x356+192+400 >/dev/null 2>&1 &"})     },
-
 		{ BTN_START,   KeyboardKey(KEY_MUTE)       },
 		{ BTN_TL,      KeyboardKey(KEY_VOLUMEDOWN) },
 		{ BTN_TR,      KeyboardKey(KEY_VOLUMEUP)   },
+
+		{ BTN_WEST,    Command({"/usr/bin/sh", "-c", Command::TOGGLE_SVKBD}) },
 	};
 }
