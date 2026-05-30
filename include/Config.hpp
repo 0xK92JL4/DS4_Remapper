e1760f83a2c6555af3df0786f04295c5a49d8a2c
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2026-05-30 18:03:44 +0200

    refactor(config): cleaner action struct

--- include/Config.hpp
+++ include/Config.hpp
@@ -5,7 +5,7 @@
 /*│  By: 0xK92JL4                                               ▒▒▒▒          │*/
 /*│                                                           ▒▒▒▒▒▒▒▒        │*/
 /*│  Created: 2026/05/17 00:58:04 by 0xK92JL4                 ▒▒▒▒▒▒▒▒        │*/
-/*│  Updated: 2026/05/29 22:15:54 by 0xK92JL4                 ▒▒    ▒▒        │*/
+/*│  Updated: 2026/05/30 17:59:23 by 0xK92JL4                 ▒▒    ▒▒        │*/
 /*│                                                                           │*/
 /*└───────────────────────────────────────────────────────────────────────────┘*/
 
@@ -38,23 +38,28 @@ namespace Config
     const char* const DS4_DEVICE = "/dev/input/event21";
     const char* const TOUCHPAD_DEVICE = "/dev/input/event23";
 
-	const char* const LED_R = "/sys/class/leds/input31:red/brightness";
-	const char* const LED_G = "/sys/class/leds/input31:green/brightness";
-	const char* const LED_B = "/sys/class/leds/input31:blue/brightness";
-	const char* const LED_GLOBAL = "/sys/class/leds/input31:global/brightness";
+	const char* const LED_R = "/sys/class/leds/input22:red/brightness";
+	const char* const LED_G = "/sys/class/leds/input22:green/brightness";
+	const char* const LED_B = "/sys/class/leds/input22:blue/brightness";
+	const char* const LED_GLOBAL = "/sys/class/leds/input22:global/brightness";
 
 	const char* const BATTERY_CAPACITY
 		= "/sys/class/power_supply/ps-controller-battery-28:c1:3c:48:43:83/capacity";
 	const char* const BATTERY_STATUS
 		= "/sys/class/power_supply/ps-controller-battery-28:c1:3c:48:43:83/status";
 
-	const std::unordered_map<int, Action> InputMap = {
-		{ BTN_SOUTH,  { ActionType::MouseButton, BTN_LEFT       } },
-		{ BTN_EAST,   { ActionType::MouseButton, BTN_RIGHT      } },
-		{ BTN_NORTH,  { ActionType::MouseButton, BTN_MIDDLE     } },
+	constexpr auto MouseButton = Action::MouseButton;
+    constexpr auto KeyboardKey = Action::KeyboardKey;
+    constexpr auto Macro       = Action::Macro;
+    constexpr auto Command     = Action::Command;
 
-		{ BTN_START,  { ActionType::KeyboardKey, KEY_MUTE       } },
-		{ BTN_TL,     { ActionType::KeyboardKey, KEY_VOLUMEDOWN } },
-		{ BTN_TR,     { ActionType::KeyboardKey, KEY_VOLUMEUP   } },
+	const std::unordered_map<int, Action> InputMap = {
+		{ BTN_SOUTH,   MouseButton(BTN_LEFT)       },
+		{ BTN_EAST,    MouseButton(BTN_RIGHT)      },
+		{ BTN_NORTH,   MouseButton(BTN_MIDDLE)     },
+
+		{ BTN_START,   KeyboardKey(KEY_MUTE)       },
+		{ BTN_TL,      KeyboardKey(KEY_VOLUMEDOWN) },
+		{ BTN_TR,      KeyboardKey(KEY_VOLUMEUP)   },
 	};
 }
--- include/structs.hpp
+++ include/structs.hpp
@@ -5,11 +5,14 @@
 /*│  By: 0xK92JL4                                               ▒▒▒▒          │*/
 /*│                                                           ▒▒▒▒▒▒▒▒        │*/
 /*│  Created: 2026/05/20 23:29:57 by 0xK92JL4                 ▒▒▒▒▒▒▒▒        │*/
-/*│  Updated: 2026/05/30 14:50:29 by 0xK92JL4                 ▒▒    ▒▒        │*/
+/*│  Updated: 2026/05/30 17:58:15 by 0xK92JL4                 ▒▒    ▒▒        │*/
 /*│                                                                           │*/
 /*└───────────────────────────────────────────────────────────────────────────┘*/
 
 #pragma once
+
+#include <vector>
+#include <string>
 
 struct Vec2
 {
@@ -27,8 +30,8 @@ enum class ActionType
 
 struct MacroTarget
 {
-    ActionType type;
-    int        code;
+	ActionType	type;
+	int			code;
 };
 
 struct Action
@@ -37,4 +40,35 @@ struct Action
 	int							code;
 	std::vector<MacroTarget>	macro_keys;
 	std::vector<std::string>	cmd_args;
+
+	Action(ActionType t, int c)
+		: type(t), code(c) {}
+
+	static Action MouseButton(int btn_code)
+	{
+		return Action(ActionType::MouseButton, btn_code);
+	}
+
+	static Action KeyboardKey(int key_code)
+	{
+		return Action(ActionType::KeyboardKey, key_code);
+	}
+
+	static Action Macro(std::initializer_list<Action> actions)
+	{
+		Action parent(ActionType::Macro, 0);
+		parent.macro_keys.reserve(actions.size());
+
+		for (const auto& act : actions)
+			parent.macro_keys.push_back({act.type, act.code});
+
+		return parent;
+	}
+
+	static Action Command(std::initializer_list<std::string> args)
+	{
+		Action a(ActionType::Command, 0);
+		a.cmd_args = args;
+		return a;
+	}
 };
